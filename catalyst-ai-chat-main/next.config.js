ed7ea85fd4fb680adf4cd7e741bebe69d13b189a
Mark W Roberts <Mark.Roberts@ey.com>
2024-10-24 18:31:46 -0400

    allowing origins from vercel

--- catalyst-ai-chat-main/next.config.js
+++ catalyst-ai-chat-main/next.config.js
@@ -15,6 +15,8 @@ module.exports = {
       allowedOrigins: [
         'localhost:3000', // localhost
         'probable-goggles-7v59g69jpj93pvqv.github.dev', // Codespaces
+        'https://catalyst-lac.vercel.app/', // Vercel
+        'https://catalyst-9dmtzxw8g-markwroberts0s-projects.vercel.app/', // Vercel
       ],
     },
   }
