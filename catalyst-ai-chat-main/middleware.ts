c58ac58084e6859028508060a729adffd4bcf808
Mark W Roberts <Mark.Roberts@ey.com>
2024-10-28 13:32:10 -0400

    revert middleware change

--- catalyst-ai-chat-main/middleware.ts
+++ catalyst-ai-chat-main/middleware.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import { authConfig } from './auth.config'
 
-export { auth as middleware } from "@/auth"
+export default NextAuth(authConfig).auth
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
